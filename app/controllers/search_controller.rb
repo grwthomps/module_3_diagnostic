04a091355f0771ce3ebc4f20fccbdd0095584e05
Graham Thompson <grwthomps@gmail.com>
2019-12-09 11:19:34 -0700

    Minor refactor: add space in view, remove unnecessary instance variable in search controller, break out fetching all members in facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @house = params[:house]
-
     render locals: {
       house: params[:house],
       members: MemberIndexFacade.new
--- app/facades/member_index_facade.rb
+++ app/facades/member_index_facade.rb
@@ -1,7 +1,10 @@
 class MemberIndexFacade
+  def get_all_members
+    PotterService.fetch_members
+  end
+
   def order_of_the_phoenix(house)
-    all_members = PotterService.fetch_members
-    ootp_members = all_members.find_all {|member| member[:orderOfThePhoenix] == true && member[:house] == house}
+    ootp_members = get_all_members.find_all {|member| member[:orderOfThePhoenix] == true && member[:house] == house}
 
     ootp_members.map do |member|
       Member.new(member)
