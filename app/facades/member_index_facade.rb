f9e361b6a319054fb34b2d59f27d52b9e2d523af
Graham Thompson <grwthomps@gmail.com>
2019-12-09 10:55:46 -0700

    Expand logic of order_of_the_phoenix method in MemberIndexFacade to call service and format results

--- app/facades/member_index_facade.rb
+++ app/facades/member_index_facade.rb
@@ -1,5 +1,10 @@
 class MemberIndexFacade
   def order_of_the_phoenix(house)
-    members = PotterService.get_members(house)
+    all_members = PotterService.fetch_members
+    ootp_members = all_members.find_all {|member| member[:orderOfThePhoenix] == true && member[:house] == house}
+
+    ootp_members.map do |member|
+      Member.new(member)
+    end
   end
 end
